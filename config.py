7978e4c16ba8fdfe2360156e291480df0f22d778
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-09-17 17:12:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 1234567))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT") in ["True", "true"]
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL")) if os.environ.get("LOGS_CHANNEL") else None
+    APP_ID = int(os.environ.get("APP_ID", "23830477"))
+    API_HASH = os.environ.get("API_HASH", "19f8365d98fb11c9cd6c1eaa8b1fa4b8")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6491987255:AAFL_prlpMzAQzvW31334AXDGnWIojHhJ0o")
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6408116706").split())
+    IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT") in ["False", "False"]
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL")) if os.environ.get("-1001888075413") else None
     # DON'T CHANGE THESE 2 VARS
     DOWNLOAD_LOCATION = "./NexaBots"
     TG_MAX_SIZE = 2040108421
     # Mega User Account
-    MEGA_EMAIL = os.environ.get("MEGA_EMAIL")
-    MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD")
+    MEGA_EMAIL = os.environ.get("MEGA_EMAIL", "githubsaif572@gmail.com")
+    MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD", "saifansari786")
